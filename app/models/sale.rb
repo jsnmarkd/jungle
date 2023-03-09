cdc29ba1d92cbd7b129c4741ffcc02d29beb18aa
Jason Divinagracia <jmarkd04@gmail.com>
2023-03-09 17:32:20 -0500

    Add Feature: Admin - Sales - part 2. Implement status in sales page.

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,15 @@
 class Sale < ApplicationRecord
+
+  def finished?
+    ends_on < Date.current
+  end
+
+  def pending?
+    starts_on > Date.current
+  end
+  
+  def active?
+    !pending? && !finished?
+  end
+
 end
